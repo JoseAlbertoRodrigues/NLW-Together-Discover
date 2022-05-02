696060d3c69b7a0c0c3ba4192e06d386af1ce306
Jose Alberto <josealberto270920@gmail.com>
2022-05-02 17:39:31 -0300

    Update buttons modal
    creating modal button

---
+++ public/scripts/main.js
@@ -0,0 +1,16 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+// Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach( button => {
+    // adicionar a escuta
+    button.addEventListener("click", event => {
+        // Abrir modal (Pegar quando o marcar como lido for clicado)
+        modal.open()
+    })
+})
+
+

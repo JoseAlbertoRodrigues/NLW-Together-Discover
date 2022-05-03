f491de650525c05733f703e2a0f2a940976b5d1f
Jose Alberto <josealberto270920@gmail.com>
2022-05-02 21:25:44 -0300

    Update main.js
    Adding the functions and events to modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,23 +2,40 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 // Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach( button => {
     // adicionar a escuta
-    button.addEventListener("click", event => {
-        // Abrir modal (Pegar quando o marcar como lido for clicado)
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 /* Quando o botão delete for clicado abre a modal */
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
+// adicionar a escuta
 deleteButton.forEach( button => {
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
 
+function handleClick(event, check = true) {
+    const text = check ? "Marcar como lida" : "Excluir"
+
+    modalTitle.innerHTML = `${text} esta pergunta`
+
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+
+    // Abrir modal (Pegar quando o marcar como lido for clicado)
+    modal.open()
+}
+
+
+// modalDescription.innerHTML = check ? "Tem certeza que deseja marcar como lida esta pergunta?" : "Tem certeza que você deseja excluir esta pergunta?"

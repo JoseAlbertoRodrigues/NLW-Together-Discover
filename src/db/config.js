239beb820fd1478a095f216aa20ec0f8bf0a5acb
Jose Alberto <josealberto270920@gmail.com>
2022-06-09 18:01:12 -0300

    installing the database
    configuring the database, we will only use the open function

---
+++ src/db/config.js
@@ -0,0 +1,8 @@
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite") //só irei precisar de uma função, que é a OPEN, sempre temos que abrir uma conexão com o banco de dados
+
+module.exports = () =>
+    open({
+        filename: '/src/db/rocketq.sqlite',
+        driver: sqlite3.Database,
+    });
